0fd61b508d9a9f17c30923e8793975b01d94d770
clifforddonk <clifforddonk@gmail.com>
2025-04-05 12:51:27 +0000

    first commit

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const metadata = {
+  title: "Clifford-Portfolio",
+  description: "Personal Web Portfolio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${nunito.variable} font-sans antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
