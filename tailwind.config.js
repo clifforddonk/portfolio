293f3bcf910e4b1c46fcbaa157f3aa4227e0a7c7
clifforddonk <clifforddonk@gmail.com>
2025-04-10 05:28:16 +0000

    add dark mode

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // 👈 enable dark mode via class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
