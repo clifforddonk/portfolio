16a96869256396fa05a422ac06752eeb03013a3e
clifforddonk <clifforddonk@gmail.com>
2025-04-12 16:24:35 +0000

    new changes

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -77,7 +77,7 @@ export default function ContactForm() {
                 }`}
               >
                 Email
-              </label>
+              </label> 
               <input
                 type="email"
                 id="email"
