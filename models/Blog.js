c0f14c639fa11111f19566d03171c22d63b69584
Clifford Aduako Donkor <clifforddonk@gmail.com>
2025-06-09 00:31:12 +0000

    Add Blog Section

---
+++ models/Blog.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    content: { type: String, required: true },
+    coverImage: { type: String },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Blog || mongoose.model("Blog", BlogSchema);
